540eb06e07bb36cb0871b90a151d237380a67a9f
nowgnoesiohc <csw6203@naver.com>
2025-02-24 15:40:44 +0900

    docs : change site configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   images: {
-    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+    domains: [
+      "www.notion.so",
+      "lh5.googleusercontent.com",
+      "s3-us-west-2.amazonaws.com",
+    ],
+  },
+  env: {
+    NOTION_PAGE_ID: process.env.NOTION_PAGE_ID,
   },
 }
--- site.config.js
+++ site.config.js
@@ -10,22 +10,22 @@ const CONFIG = {
     github: "nowgnoesiohc",
     instagram: "",
   },
-  projects: [
-    {
-      name: `morethan-log`,
-      href: "https://github.com/nowgnoesiohc/notion-blog",
-    },
-  ],
+  // projects: [
+  //   {
+  //     name: `morethan-log`,
+  //     href: "https://github.com/nowgnoesiohc/notion-blog",
+  //   },
+  // ],
   // blog setting (required)
   blog: {
     title: "Put my heart into",
-    description: "welcome to morethan-log!",
+    description: "Put my heart into development!",
     scheme: "system", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://castle1.site",
+  since: 2025, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
